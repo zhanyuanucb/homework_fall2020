c4d4f48ba4b5788965add93fb271739f2e9b0492
zhanyuanucb <zhang_zhanyuan@berkeley.edu>
2020-10-18 04:33:57 -0700

    paste utils.py

--- hw3/cs285/infrastructure/utils.py
+++ hw3/cs285/infrastructure/utils.py
@@ -54,82 +54,65 @@ def mean_squared_error(a, b):
 
 ############################################
 ############################################
-
 def sample_trajectory(env, policy, max_path_length, render=False, render_mode=('rgb_array')):
-
-    # initialize env for the beginning of a new rollout
-    ob = env.reset() # HINT: should be the output of resetting the env
-
-    # init vars
+    ob = env.reset()
     obs, acs, rewards, next_obs, terminals, image_obs = [], [], [], [], [], []
     steps = 0
     while True:
-
-        # render image of the simulated env
-        if render:
+        if render:  # feel free to ignore this for now
             if 'rgb_array' in render_mode:
-                if hasattr(env, 'sim'):
-                    image_obs.append(env.sim.render(camera_name='track', height=500, width=500)[::-1])
+                if hasattr(env.unwrapped, 'sim'):
+                    if 'track' in env.unwrapped.model.camera_names:
+                        image_obs.append(env.unwrapped.sim.render(camera_name='track', height=500, width=500)[::-1])
+                    else:
+                        image_obs.append(env.unwrapped.sim.render(height=500, width=500)[::-1])
                 else:
                     image_obs.append(env.render(mode=render_mode))
             if 'human' in render_mode:
                 env.render(mode=render_mode)
                 time.sleep(env.model.opt.timestep)
-
-        # use the most recent ob to decide what to do
         obs.append(ob)
-        with torch.no_grad():
-            ac = policy.get_action(ob) # HINT: query the policy's get_action function
+        ac = policy.get_action(ob)
         ac = ac[0]
         acs.append(ac)
-
-        # take that action and record results
         ob, rew, done, _ = env.step(ac)
-
-        # record result of taking that action
-        steps += 1
+        # add the observation after taking a step to next_obs
         next_obs.append(ob)
         rewards.append(rew)
-
-        # TODO end the rollout if the rollout ended
-        # HINT: rollout can end due to done, or due to max_path_length
-        rollout_done = 1 if done or steps >= max_path_length else 0 # HINT: this is either 0 or 1
-        terminals.append(rollout_done)
-
-        if rollout_done:
+        steps += 1
+        # If the episode ended, the corresponding terminal value is 1
+        # otherwise, it is 0
+        if done or steps > max_path_length:
+            terminals.append(1)
             break
-
+        else:
+            terminals.append(0)
     return Path(obs, image_obs, acs, rewards, next_obs, terminals)
-    
-def sample_trajectories(env, policy, min_timesteps_per_batch, max_path_length, render=False, render_mode=('rgb_array')):
-    """
-        Collect rollouts until we have collected min_timesteps_per_batch steps.
 
-        TODO implement this function
-        Hint1: use sample_trajectory to get each path (i.e. rollout) that goes into paths
-        Hint2: use get_pathlength to count the timesteps collected in each path
-    """
+def sample_trajectories(env, policy, min_timesteps_per_batch, max_path_length, render=False, render_mode=('rgb_array')):
+
     timesteps_this_batch = 0
     paths = []
     while timesteps_this_batch < min_timesteps_per_batch:
 
-        cur_path = sample_trajectory(env, policy, max_path_length, render=render, render_mode=render_mode)
-        timesteps_this_batch += get_pathlength(cur_path)
-        paths.append(cur_path)
+        #collect rollout
+        path = sample_trajectory(env, policy, max_path_length, render, render_mode)
+        paths.append(path)
+
+        #count steps
+        timesteps_this_batch += get_pathlength(path)
+        print('At timestep:    ', timesteps_this_batch, '/', min_timesteps_per_batch, end='\r')
 
     return paths, timesteps_this_batch
 
 def sample_n_trajectories(env, policy, ntraj, max_path_length, render=False, render_mode=('rgb_array')):
-    """
-        Collect ntraj rollouts.
 
-        TODO implement this function
-        Hint1: use sample_trajectory to get each path (i.e. rollout) that goes into paths
-    """
     paths = []
+    for i in range(ntraj):
+        # collect rollout
+        path = sample_trajectory(env, policy, max_path_length, render, render_mode)
+        paths.append(path)
 
-    for _ in range(ntraj):
-        paths.append(sample_trajectory(env, policy, ntraj, max_path_length, render=render, render_mode=render_mode))
     return paths
 
 ############################################
